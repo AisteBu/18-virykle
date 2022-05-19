78d1be0817330e53a9ad1972917c5b4c575f5b5b
AisteB <aistebuneviciene@gmail.com>
2022-05-19 05:07:07 +0200

    Render

--- js/components/Virykle.js
+++ js/components/Virykle.js
@@ -1,10 +1,14 @@
 class Virykle {
     constructor (selector, elementsCount) {  //gaunam info
         this.selector = selector;           
-        this.selector = elementsCount;     // kaitlentes pavirsiai / kaitvietes  ...su siais dviem issisaugau info
+        this.elementsCount = elementsCount;     // kaitlentes pavirsiai / kaitvietes  ...su siais dviem issisaugau info
 
         this.DOM = null;
-        this.price = 1000;                  
+        this.price = 1000; 
+        this.proportion = {
+            x: 1,
+            y: 1
+        }                 
 
         this.init();                        //pradedam kazka inicijuoti
     }
@@ -16,6 +20,7 @@ class Virykle {
             return false;
         }
 
+        this.calcProportions();
         this.render();                      //logikos isskaidymas i kita dali 
     }
 
@@ -69,19 +74,44 @@ class Virykle {
         return true;
     }
 
+    calcProportions() {
+        this.proportion.x = Math.ceil(Math.sqrt(this.elementsCount))
+        this.proportion.y = Math.ceil(this.elementsCount / this.proportion.x)
+    }
+
+    generateElements() {
+        let HTML = '';
+
+        for (let i=0; i < this.elementsCount; i++) {
+            HTML += '<div class="kaitlente"></div>';
+        }
+
+        return HTML;
+    }
+
+    generateSwitches() {
+        let HTML = '';
+
+        for (let i=0; i < this.elementsCount; i++) {
+            HTML += '<div class="jungiklis"></div>';
+        }
+
+        return HTML;
+    }
+
     render() {
-        let HTML = `<div class="virykle">
+        const elementWidth = 120;
+        const elementMargin = 10;
+        const fullElementWidth = elementWidth + elementMargin * 2;
+        const rangesBorderWidth = 1;
+        const width = fullElementWidth * this.proportion.x + rangesBorderWidth * 2;
+
+        const HTML = `<div class="virykle" style="width: ${width}px;">
                         <div class="kaitlentes">
-                            <div class="kaitlente"></div>
-                            <div class="kaitlente"></div>
-                            <div class="kaitlente"></div>
-                            <div class="kaitlente"></div>
+                            ${this.generateElements()}
                         </div>
                         <div class="jungikliai">
-                            <div class="jungiklis"></div>
-                            <div class="jungiklis"></div>
-                            <div class="jungiklis"></div>
-                            <div class="jungiklis"></div>
+                            ${this.generateSwitches()}
                         </div>
                     </div> `;
 
@@ -90,3 +120,42 @@ class Virykle {
 }
 
 export { Virykle }
+
+
+/*
+
+SKYLES ISDESTYMAS:
+1       1X1
+2       2X1
+3       2x2
+4       2x2
+5       3x2
+6       3x2
+7       3x3
+8       3x3
+9       3x3
+10      4x3
+11      4x3
+12      4x3
+13      4x4
+14      4x4
+15      4x4
+16      4x4
+
+Pirmasis skaicius:
+
+n - skyliu kiekis
+ceil - apvalinam i virsu
+
+x = Math.ceil(Math.sqrt(n))         gausime, kiek reikia i ploti sudeti skyliu
+y = Math.ceil(n / x)
+
+n = 7
+x = 3                                saknis is 7 yra 2.645
+y = 3                               7 / 3 = 2.33333
+
+n = 10
+x = 4                                saknis is 10 yra 3.16
+y = 3                               10 / 4 yra 2.5
+
+*/
